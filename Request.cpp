ec6b4abd828f82493fabb27c9c161489ea886f7c
Abla Ouhaga <abouhaga@c4r4p9.1337.ma>
2023-09-03 15:37:22 +0100

    last smol modif

--- Request.cpp
+++ Request.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Request.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybel-hac <ybel-hac@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abouhaga <abouhaga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 16:57:48 by abouhaga          #+#    #+#             */
-/*   Updated: 2023/09/03 12:06:12 by ybel-hac         ###   ########.fr       */
+/*   Updated: 2023/09/03 15:36:41 by abouhaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -179,7 +179,7 @@ bool RequestErrors(Request &request, Client &client)
         return  0;
     }
 
-    if (method == "POST" && contentLengthStr.empty() && transferEncoding.empty()) {
+    if (method == "POST" && (contentLengthStr.empty() || transferEncoding.empty())) {
         client.setStatus(400);
         return 0;
     }
